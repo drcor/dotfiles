396db497fac7ff7f26a2f076d9731ec5650234eb
drcor <53583855+drcor@users.noreply.github.com>
2023-02-09 23:36:19 +0000

    Add Fish Shell configurations
    Set colors, variables, alias, abbr's and starship initialization call

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+set fish_greeting   # Remove default fish startup message
+
+export EDITOR=nvim || export EDITOR=vim
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+
+# Import configuration files
+source $HOME/.config/fish/shortcuts.fish
+source $HOME/.config/fish/colors.fish
+
+alias la='exa -lha' # List directories with color
+alias df='df -h'
+alias du='du -ch'
+alias emacs='emacs -nw'
+
+# Initialize starship
+starship init fish | source
+
+# Print startup message
+echo -e "\n"
+neofetch
---
+++ fish/.config/fish/shortcuts.fish
@@ -0,0 +1,3 @@
+abbr vimconf "nvim ~/.vimrc"
+abbr mkdir "mkdir -p"
+abbr serverpy "python3 -m http.server"
