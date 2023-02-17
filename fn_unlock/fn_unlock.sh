96a6c8bff8ddf284ef3f8f9cb529e151429f07bd
Diogo Correia <53583855+drcor@users.noreply.github.com>
2023-02-17 23:06:49 +0000

    Update fn_unlock script to support Manjaro

--- fn_unlock/fn_unlock.sh
+++ fn_unlock/fn_unlock.sh
@@ -6,6 +6,20 @@ L=/etc/modprobe.d/alsa-base.conf;
 E="\n** $G $H";
 F=" **\n"
 
+# Get distribution name
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+elif type lsb_release >/dev/null 2>&1; then
+    OS=$(lsb_release -si)
+elif [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+fi
+
+echo -e "$OS"
+
 if [ -f /sys/module/$G/parameters/$H ]; then
     I="INSTALLED"
     grep -q "$H=N" $L
@@ -16,10 +30,26 @@ if [ -f /sys/module/$G/parameters/$H ]; then
     O="options $G ${H}=N"
     echo -e "#Toggle $H at boot (Y/N)\n$O\n" | sudo tee -a $L
     echo -e "\nPlease WAIT ...\n"
-    sudo mkinitrd -k all
+
+    case $OS in
+        "openSUSE Tumbleweed")
+            sudo mkinitrd -k all
+            ;;
+
+        "ManjaroLinux" | "Manjaro Linux" | "Arch")
+            sudo mkinitcpio -P
+            ;;
+
+        *)
+            echo "Operating system not suported!!"
+            exit 1
+            ;;
+    esac
+
     echo -e "$E $I NOW$F"
     exit 0
 fi
+
 echo -e "$E NOT FOUND$F"
 exit 1
-#
+
